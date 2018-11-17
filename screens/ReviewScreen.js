c96937ebaf7cbe3b6b23e588fdd53c9fc9e1578b
Dean Glueckler <glueckler@gmail.com>
2018-11-16 18:04:59 -0800

    added basic top bar navigation

--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
 
+import { Button } from 'react-native-elements'
+
 export default class ReviewScreen extends Component {
-  // When react navigation renders a component it will look at
-  // the static 'navigationOptions' property on the class
-  static navigationOptions = {
+  // use a funtion to interact with site navition
+  static navigationOptions = ({ navigation }) => ({
     title: 'Review Screen', // config the header title
-    headerRight: <Text>Go Right</Text>, // displayed on right side
-  }
+    headerRight: (
+      <Button
+        title="Settings"
+        onPress={() => navigation.navigate('settings')}
+      />
+    ),
+  })
+
   render() {
     return (
       <View>
