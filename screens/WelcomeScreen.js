5ec18386c6f917dee126993c59b90bb74f819dd0
Dean Glueckler <glueckler@gmail.com>
2018-11-16 18:22:05 -0800

    slides scaffold

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
 
+import Slides from '../components/Slides'
+
+const SLIDES = [
+  { text: 'Welcome to JobApp' },
+  { text: 'Set your location, then swipe away' },
+]
+
 export default class WelcomeScreen extends Component {
   render() {
-    return (
-      <View>
-        <Text> Welcome Screen </Text>
-      </View>
-    )
+    return <Slides data={SLIDES} />
   }
 }
