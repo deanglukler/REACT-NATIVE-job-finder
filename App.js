d57572e0dca1352b791b86b0ba1bf73089422654
Dean Glueckler <glueckler@gmail.com>
2018-11-16 17:41:31 -0800

    add boilerplate component and navigation

--- App.js
+++ App.js
@@ -1,13 +1,40 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { PureComponent } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import {
+  createBottomTabNavigator,
+  createStackNavigator,
+} from 'react-navigation'
 
-export default class App extends React.Component {
+import AuthScreen from './screens/AuthScreen'
+import WelcomeScreen from './screens/WelcomeScreen'
+import MapScreen from './screens/MapScreen'
+import DeckScreen from './screens/DeckScreen'
+import SettingsScreen from './screens/SettingsScreen'
+import ReviewScreen from './screens/ReviewScreen'
+const MainNavigator = createBottomTabNavigator({
+  Welcome: { screen: WelcomeScreen },
+  Auth: { screen: AuthScreen },
+  main: {
+    screen: createBottomTabNavigator({
+      map: { screen: MapScreen },
+      deck: { screen: DeckScreen },
+      review: {
+        screen: createStackNavigator({
+          settings: { screen: SettingsScreen },
+          review: { screen: ReviewScreen },
+        }),
+      },
+    }),
+  },
+})
+
+export default class App extends PureComponent {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <MainNavigator />
       </View>
-    );
+    )
   }
 }
 
@@ -15,7 +42,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-});
+})
