31a8fef5d9b0943240f88a6d5a09468f15d09d3f
dean <dbean@Tests-MacBook-Pro.local>
2018-11-17 17:18:39 -0800

    check for auth token

--- reducers/auth_reducer.js
+++ reducers/auth_reducer.js
@@ -1,8 +1,8 @@
 import { FB_LOGIN_SUCCESS, FB_LOGIN_FAIL } from '../actions/types'
 
 export default (state = {}, action) => {
-  switch (action) {
-    case FB_LOGIN_SUCCESS:
+  switch (action.type) {
+    case FB_LOGIN_SUCCESS: 
       return { token: action.payload }
     case FB_LOGIN_FAIL:
       return { token: null }
--- screens/AuthScreen.js
+++ screens/AuthScreen.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, AsyncStorage } from 'react-native'
 import { connect } from 'react-redux'
 import * as actions from '../actions'
 
-const hoc = c => {
-  return connect(null, actions)(c)
-}
+const hoc = c => connect(s => ({ token: s.auth.token }), actions)(c)
 
 export default hoc(class AuthScreen extends Component {
   componentWillMount() {
+    // to clear storage for testing
+    // AsyncStorage.clear() 
+    
     this.props.facebookLogin()
+    // this isn't really necessary (maybe) because we are also checking in componentWillReceive
+    this.onAuthComplete(this.props)
   }
+
+  componentWillReceiveProps(nextProps) {
+    this.onAuthComplete(nextProps)
+  }
+
+  onAuthComplete(props) {
+    if (props.token) {
+      this.props.navigation.navigate('map')
+    }
+  }
+  
   render() {
     return (
       <View>
