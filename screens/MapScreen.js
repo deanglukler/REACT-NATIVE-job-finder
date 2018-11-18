3718709ccb92d00caa313f20eea42fa543f60879
dean <dbean@Tests-MacBook-Pro.local>
2018-11-17 17:51:00 -0800

    added map

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
+import { MapView } from 'expo'
 
 export default class MapScreen extends Component {
+  state = {
+    region: {
+      longitude: -122,
+      latitude: 37,
+      longitudeDelta: 0.04,
+      latitudeDelta: 0.09,
+    }
+  }
+  
   render() {
     return (
-      <View>
-        <Text> Map Screen </Text>
+      <View style={{ flex: 1 }} >
+        <MapView region={this.state.region} style={{ flex: 1 }} />
       </View>
     )
   }
