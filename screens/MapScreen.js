dc152baf856178f9d92d107aad8ea1cc9d0c519a
dean <dbean@Tests-MBP.hitronhub.home>
2018-11-18 16:19:10 -0800

    added android load compatibility fix

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,22 +1,32 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
-import { MapView } from 'expo'
+import React, { Component } from "react";
+import { Text, View, ActivityIndicator } from "react-native";
+import { MapView } from "expo";
 
 export default class MapScreen extends Component {
   state = {
+    // mapLoaded is a work around for android systems that don't render correctly on load
+    mapLoaded: false,
     region: {
       longitude: -122,
       latitude: 37,
       longitudeDelta: 0.04,
-      latitudeDelta: 0.09,
+      latitudeDelta: 0.09
     }
+  };
+
+  componentDidMount() {
+    this.setState({
+      mapLoaded: true,
+    })
   }
-  
+
   render() {
     return (
-      <View style={{ flex: 1 }} >
-        <MapView region={this.state.region} style={{ flex: 1 }} />
+      <View style={{ flex: 1 }}>
+        {this.state.mapLoaded ? (
+          <MapView region={this.state.region} style={{ flex: 1 }} />
+        ) : <ActivityIndicator size="large" />}
       </View>
-    )
+    );
   }
 }
