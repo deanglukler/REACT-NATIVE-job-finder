23f0df5faa714c437ea2211a510921e4357fa93f
dean <dbean@Tests-MacBook-Pro.local>
2018-11-19 18:42:32 -0800

    built list of map cards

--- screens/DeckScreen.js
+++ screens/DeckScreen.js
@@ -1,21 +1,72 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
-import { connect } from 'react-redux'
+import React, { Component } from "react";
+import { Text, View, Platform } from "react-native";
+import { connect } from "react-redux";
+import { MapView } from "expo";
+import { Card, Button } from "react-native-elements";
+
+import Swipe from "../components/Swipe";
 
 const mapState = ({ jobs }) => {
   return {
-    jobs: jobs.results,
-  }
-}
+    jobs: jobs.results
+  };
+};
 
-const hoc = c => connect(mapState)(c)
+const hoc = c => connect(mapState)(c);
 
-export default hoc(class DeckScreen extends Component {
-  render() {
-    return (
-      <View>
-        <Text> Deck Screen </Text>
-      </View>
-    )
+export default hoc(
+  class DeckScreen extends Component {
+    renderCard(job) {
+      const initialRegion = {
+        longitude: job.longitude,
+        latitude: job.latitude,
+        latitudeDelta: 0.045,
+        longitudeDelta: 0.02
+      };
+
+      return (
+        <Card title={job.jobtitle}>
+          <View style={{ height: 300 }}>
+            <MapView
+              scrollEnabled={false}
+              style={{ flex: 1 }}
+              initialRegion={initialRegion}
+              // cache enabled will display map as an image which could help performance
+              // it also helps android not glitch out so much when displaying multiple maps
+              cacheEnabled={Platform.OS === "android" ? true : false}
+            />
+          </View>
+          <View style={styles.detailWrapper}>
+            <Text>{job.company}</Text>
+            <Text>{job.formattedRelativeTime}</Text>
+          </View>
+          <Text>{job.snippet.replace(/[<b>|</b>]/g, "")}</Text>
+        </Card>
+      );
+    }
+
+    renderNoMoreCards() {
+      return <Card title="No more cards" />;
+    }
+
+    render() {
+      return (
+        <View style={{ marginTop: 40 }}>
+          <Swipe
+            useKeyPropName="jobkey"
+            data={this.props.jobs}
+            renderCard={this.renderCard}
+            renderNoMoreCards={this.renderNoMoreCards}
+          />
+        </View>
+      );
+    }
   }
-})
+);
+
+const styles = {
+  detailWrapper: {
+    flexDirection: "row",
+    justifyContent: "space-around"
+  }
+};
